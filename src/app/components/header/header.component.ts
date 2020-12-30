c77caaec2d21e331d6bc536827585b32fea552b6
Nino Righi <deidrox.business@t-online.de>
2020-12-30 13:26:33 +0100

    Added start button and styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: '<h1>Snake Classic v1.0</h1><h2>- for mobile & desktop -</h2>',
-  styles: ['h1,h2 {text-align: center;}'],
+  styles: ['h1,h2 {text-align: center; text-shadow: 4px 4px 8px black}'],
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
